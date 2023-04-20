584b65468e706725f18a1026cf871d334de36e47
Muhammed Fahiz <fahizp3@gmail.com>
2023-04-20 09:15:01 +0530

    ADD THE AUTH SERVICE
    setup the suth services and also users end points

---
+++ auth-service/src/middleware/index.ts
@@ -0,0 +1,46 @@
+import express from 'express';
+import { merge, get } from 'lodash';
+
+import { getUserBySessionToken } from '../db/users';
+
+
+export const isOwner = async (req: express.Request, res:express.Response, next: express.NextFunction) => {
+    try {
+        const { id } = req.params;
+
+        const sessionToken = req.cookies['FAHIZ-AUTH'];
+        
+        const currentUserId  = await getUserBySessionToken(sessionToken);
+
+
+        if(!currentUserId)  res.sendStatus(400);
+
+        if(currentUserId?._id.toString() !== id) return res.sendStatus(403);
+        
+        return next();
+    } catch (error) {
+        console.log(error);
+        
+        return res.sendStatus(400);
+    }
+}
+
+export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const sessionToken = req.cookies['FAHIZ-AUTH'];
+
+        if(!sessionToken) res.sendStatus(403);
+
+        const existingUser = await getUserBySessionToken(sessionToken);
+
+        if(!existingUser) res.sendStatus(403);
+
+        merge(req, { identity: existingUser });
+
+        return next();
+    } catch (error) {
+        console.log(error);
+
+        return res.sendStatus(400)
+    }
+}
---
+++ auth-service/src/router/authentication.ts
@@ -0,0 +1,9 @@
+import { register, login} from '../controllers/authentication';
+import express  from 'express';
+
+
+export default (router: express.Router) => {
+    router.post('/auth/register', register);
+
+    router.post('/auth/login', login);
+}
